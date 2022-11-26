0986e3d0473cafda2ac8625e2bc87f5bff20d3b1
Rufluenza <NEXT5177@edu.nextkbh.dk>
2022-11-26 22:11:34 +0100

    Update - shows error but everything is fine

--- website.py
+++ website.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import os
 
-
-password = 'pswd'
-
 dev = {}
 devs = {}
 def get_usb():
@@ -18,20 +15,23 @@ def get_usb():
     return devs
 
 
-
 st.header("Xmount eject")
 st.subheader("Eject your USB devices")
 def main():
-
     for i in range(len(get_usb())):
         # all_names = get_name()[i]
+        l = str(i)
         st.write(get_usb()[i+1].split('/')[-1])
         st.container() # make a container for each device
-        if st.button('Eject' + ' ' + get_usb()[i+1].split('/')[-1]):
+        l = st.button('Eject' + ' ' + get_usb()[i+1].split('/')[-1])
+        if l:
             st.write('ejecting'+ ' ' + get_usb()[i+1])
-            if os.system('echo '+password+' | sudo -S eject -F '+get_usb()[i+1]) == 0: # if the command is successful maybe delete the if statement "sudo -S umount" on linux
+            os.system('udisksctl unmount --block-device '+get_usb()[i+1])
+            if os.system('udisksctl power-off --block-device '+get_usb()[i+1]) == 0:  # if os.system('echo '+password+' | sudo -S eject -F '+get_usb()[i+1]) == 0: 
                 st.write('Ejected'+ ' ' + get_usb()[i+1].split('/')[-1])
-                st.write('Reload the page to see the changes')
+                st.write('Reload the page to see the changes, currently there is shown an error.\n\n But it has nothing to do with the device media.  ')
+                # refresh the page from the server side so no error is shown
 
+    
 if __name__ == "__main__":
     main()
