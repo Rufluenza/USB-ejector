cc4cd194ab5d6f65f51e99017eb2c24d3ce4b513
Rufluenza <NEXT5177@edu.nextkbh.dk>
2022-12-05 16:13:04 +0100

    Works as intended

--- website.py
+++ website.py
@@ -1,19 +1,26 @@
 import streamlit as st
 import os
+import time
 
 dev = {}
 devs = {}
-media_path = r'/media/xmount/'
+media_path = r'/dev/disk/by-label/'
 
 def get_usb():
     i = 0
     if os.path.exists(media_path):
         for dev in os.listdir(media_path):
-            i += 1
-            l = media_path+dev
-            devs[i] = l
+            if dev != 'EFI' or dev != 'ESP':
+                i += 1
+                dev = dev.strip(media_path)
+                if " " in dev:
+                    dev = r"'" + dev + r"'"
+                dev = str((dev).replace(' ', r'\x20'))
+                l = media_path+dev
+                devs[i] = l
 
     return devs
+
 
 st.header("Xmount eject")
 st.subheader("Eject your USB devices")
@@ -27,13 +34,27 @@ def main():
             st.write('ejecting'+ ' ' + get_usb()[i+1])
             path = get_usb()[i+1]
             print(path)
-            path.replace(' ', '\x') #Det her må jeg ikke. Kender du en løsning? Jeg ønsker at erstatte mellemrummet med \x, da det er det Ubuntu gør med mellemrum i diskenes navne i /dev/disk
-            os.system('udisksctl unmount --block-device '+'/dev/disk/by-label/'+path.split(media_path)[-1])
-            if os.system('udisksctl power-off --block-device '+'/dev/disk/by-label/'+path.split(media_path)[-1]) == 0:
-                st.write('Ejected'+ ' ' + get_usb()[i+1].split(media_path)[-1])
-                st.write('Reload the page to see the changes.  ')
-                # refresh the page from the server side
+            if True:
+                sd_path = os.popen("udisksctl unmount --block-device "+media_path+"'"+path.split(media_path)[-1]+"'").read()
+                sd_path = str(sd_path)
+                sd_path = sd_path.replace('Unmounted ', '')
+                sd_path = sd_path.replace('.', '')
+                sd_path = sd_path.replace(' ', '')
+                sd_path = sd_path[:-1]
+                for i in range(1, 7):
+                    sd_path = sd_path[:-1]+str(i)
+                    print(sd_path)
+                    time.sleep(1)
+                    if os.system("udisksctl unmount --block-device "+sd_path) == 0:
+                        print('unmounted')
+                        break
+                print(sd_path)
+                if os.system("udisksctl power-off --block-device "+ sd_path) == 0:
+                    st.write('Ejected'+ ' ' + sd_path)# get_usb()[i+1].split(media_path)[-1])
+                    st.write('Reload the page to see the changes.  ')
+            
 
     
 if __name__ == "__main__":
     main()
+
