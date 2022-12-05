60b2a43060c32ba1da820279ad25bfed01f2c004
Rufluenza <NEXT5177@edu.nextkbh.dk>
2022-12-05 16:17:44 +0100

    Fixed and done :D

--- website.py
+++ website.py
@@ -34,24 +34,24 @@ def main():
             st.write('ejecting'+ ' ' + get_usb()[i+1])
             path = get_usb()[i+1]
             print(path)
-            if True:
-                sd_path = os.popen("udisksctl unmount --block-device "+media_path+"'"+path.split(media_path)[-1]+"'").read()
-                sd_path = str(sd_path)
-                sd_path = sd_path.replace('Unmounted ', '')
-                sd_path = sd_path.replace('.', '')
-                sd_path = sd_path.replace(' ', '')
-                sd_path = sd_path[:-1]
-                for i in range(1, 7):
-                    sd_path = sd_path[:-1]+str(i)
-                    print(sd_path)
-                    time.sleep(1)
-                    if os.system("udisksctl unmount --block-device "+sd_path) == 0:
-                        print('unmounted')
-                        break
+            
+            sd_path = os.popen("udisksctl unmount --block-device "+media_path+"'"+path.split(media_path)[-1]+"'").read()
+            sd_path = str(sd_path)
+            sd_path = sd_path.replace('Unmounted ', '')
+            sd_path = sd_path.replace('.', '')
+            sd_path = sd_path.replace(' ', '')
+            sd_path = sd_path[:-1]
+            for i in range(1, 7):
+                sd_path = sd_path[:-1]+str(i)
                 print(sd_path)
-                if os.system("udisksctl power-off --block-device "+ sd_path) == 0:
-                    st.write('Ejected'+ ' ' + sd_path)
-                    st.write('Reload the page to see the changes.  ')
+                time.sleep(1)
+                if os.system("udisksctl unmount --block-device "+sd_path) == 0:
+                    print('unmounted')
+                    break
+            print(sd_path)
+            if os.system("udisksctl power-off --block-device "+ sd_path) == 0:
+                st.write('Ejected'+ ' ' + sd_path)
+                st.write('Reload the page to see the changes.  ')
             
 
     
